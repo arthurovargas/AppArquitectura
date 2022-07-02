12718e3b53ab9b2915ef61e97bd97d033792a0d6
unknown <Uniminuto@RSP-L90304-T-01.UMD.LOCAL.EDU>
2022-07-02 15:22:57 -0500

    Data Binding: integrando ViewModel a View

--- app/src/main/java/com/example/apparquitectura/view/MainActivity.kt
+++ app/src/main/java/com/example/apparquitectura/view/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.apparquitectura.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.apparquitectura.R
+import com.example.apparquitectura.databinding.ActivityMainBinding
+import com.example.apparquitectura.model.Coupon
+import com.example.apparquitectura.viewmodel.CouponViewmodel
 
 class MainActivity : AppCompatActivity() {
 
-    private var rvCoupons: RecyclerView? = null
+    private var couponViewmodel: CouponViewmodel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        //Generate list of coupon offers - View
-        rvCoupons = findViewById(R.id.rvCoupons)
-        rvCoupons?.layoutManager = LinearLayoutManager(this)
+        setupBindings(savedInstanceState)
+    }
+
+    fun setupBindings(savedInstanceState: Bundle?){
+        val activityMainBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this,R.layout.activity_main )
+        couponViewmodel = ViewModelProvider.NewInstanceFactory().create(CouponViewmodel::class.java)
+        activityMainBinding.model
+        setupListUpdate()
+    }
+
+    private fun setupListUpdate(){
+        couponViewmodel?.callCoupons()
+        couponViewmodel?.getCoupons()?.observe(this, Observer {
+                coupon:List<Coupon> -> Log.w("COUPON", coupon[0].title)
+        })
     }
 }
